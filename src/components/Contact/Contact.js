12c807eb48e38c8a5e46639117682a927b563ce4
Peter DeMaio <pedemaio@gmail.com>
2020-01-14 10:39:02 -0600

    got everything working

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>867-5309</p>
+                <p>Jenny@gmail.com</p>
+            </div>
+        )    
+    }
+}
+
+export default Contact;
